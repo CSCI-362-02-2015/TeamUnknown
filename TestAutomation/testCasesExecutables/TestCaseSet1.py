477896052bcd1acba6752a8737129e0073d06d54
Aaron <aaron.m.gaynor@gmail.com>
2015-10-29 20:58:37 -0400

    Added Documentation

--- TestAutomation/testCasesExecutables/TestCaseSet1.py
+++ TestAutomation/testCasesExecutables/TestCaseSet1.py
@@ -2,6 +2,7 @@
 
 import os, sys
 
+#get Directory for functions
 currentworkingdirectory = os.getcwd()
 currentworkingdirectory = currentworkingdirectory.replace('/testCasesExecutables', '')
 os.chdir(currentworkingdirectory)
@@ -11,20 +12,28 @@ sys.path.insert(0, currentworkingdirectory)
 
 from functions import add
 
+#get Directory for driver scripts
 currentworkingdirectory = currentworkingdirectory.replace('/project/src', '')
 currentworkingdirectory = (currentworkingdirectory + '/scripts')
 
 sys.path.insert(0, currentworkingdirectory)
 
 #from htmlConverter import writeFile
-#from filereader import reader
+from TestCaseReader import testCaseExtractor
 
-inFile = "testCaseInput" + sys.argv[1]
+#get file directory
+currentworkingdirectory = currentworkingdirectory.replace('/scripts', '')
+currentworkingdirectory = (currentworkingdirectory + '/oracles')
+
+#create file pointers
+inFile = currentworkingdirectory + "/" + "testCaseInput" + sys.argv[1]
 print(inFile)
 outFile = "testCaseOutput" + sys.argv[1]
 print(outFile)
-#x,y = reader(inFile)
-output = add(4,5)
+
+#get values from inputfile and add
+x,y = testCaseExtractor(inFile)
+output = add(int(x),int(y))
 
 #writeFile(outFile, output)
 
